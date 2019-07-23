0fba656e8b9b8acb52584262ffc1035761b8a10b
Gyasi Grey <gyasi.grey@gmail.com>
2019-07-22 20:53:50 -0400

    Updated ScoreMagicCards.py to get the data straight from the database

--- ScoreMagicCards.py
+++ ScoreMagicCards.py
@@ -1,21 +1,15 @@
+import pyodbc
 import tensorflow as tf
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
 
-#Get the Magic Card dataset
-column_names = ['CardId', 'Name', 'RulesText', 'CMC', 'Power', 'Toughness', 'Type', 'MultiverseId', 'Rating']
-raw_dataset = pd.read_csv('MagicCards.csv', names=column_names, na_values = "?", comment='\t', sep=",", skipinitialspace=True, skiprows=1, encoding='utf-8')
+#Get the Magic Card dataset from the SQL Database
+sql_conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER=localhost\SQLEXPRESS; DATABASE=MagicCards;Trusted_Connection=yes')
+query = "SELECT CardId, Name, RulesText, CMC, Power, Toughness, Type, Rating FROM Cards"
+dataset = pd.read_sql(query, sql_conn)
 
-dataset = raw_dataset.copy()
 
-
-#Drop unknown rows
-dataset = dataset.dropna()
-
-#Toss the MultiverseId column
-dataset.pop('MultiverseId')
-
-print(dataset['RulesText'].split())
+#print(dataset['RulesText'].split())
 
 #df = pd.DataFrame(pdf.Series(dict([(token, 1) for token in dataset['RulesText'].split()])), columns=['sent']).T
