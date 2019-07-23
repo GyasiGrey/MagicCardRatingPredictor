d7fb44c68b0ef75bec3ce6f649f4e2c5f84aff2d
Gyasi Grey <gyasi.grey@gmail.com>
2019-07-22 23:17:44 -0400

    Added the rest of the ML stuff that doesn't work yet

--- ScoreMagicCards.py
+++ ScoreMagicCards.py
@@ -23,10 +23,26 @@ sql_conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server}; SERVER=localh
 query = "SELECT Name, RulesText, CMC, Power, Toughness, Type, Rating FROM Cards"
 dataset = pd.read_sql(query, sql_conn)
 
+#Only include cards with a CMC of <= 20
+#Because GleeMax HAS A CMC OF 1000000 AND IT THROWS OFF THE MATH COME ON GUYS.
+dataset = dataset[dataset.CMC <= 20]
+
+
 #Build the vocabulary from Type
 types = dataset.pop('Type')
 names = dataset.pop('Name')
 rules = dataset.pop('RulesText')
+
+#Changes all nones to 0.
+#All power/toughness of NULL (All noncreature cards)
+#All ratings of NULL
+dataset.fillna(value=0, inplace=True)
+
+#Convert the ratings to integers
+ratings = dataset.pop('Rating') * 1000
+
+#Re-add the ratings to the dataset
+dataset["Rating"] = ratings.astype(int)
 
 #Remove the emdash from types
 types = types.str.replace(" — ", " ")
@@ -88,4 +104,79 @@ dataset['Name'] = namesInt
 #Add the rules as integers
 dataset['RulesText'] = rulesInt
 
-print(dataset)
+#Split out the train and test data
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+train_stats = train_dataset.describe()
+train_stats.pop("Rating")
+train_stats = train_stats.transpose()
+
+#split the labels out
+train_labels = train_dataset.pop('Rating')
+test_labels = test_dataset.pop('Rating')
+
+
+#normalize the data
+def norm(x):
+  return (x - train_stats['mean']) / train_stats['std']
+
+#OH NO: no normed data  
+#normed_train_data = norm(train_dataset)
+#normed_test_data = norm(test_dataset)
+
+
+#build the model
+def build_model():
+  model = keras.Sequential([
+    layers.Dense(64, activation=tf.nn.relu, input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation=tf.nn.relu),
+    layers.Dense(1)
+  ])
+  
+  optimizer = tf.keras.optimizers.RMSprop(0.001)
+  
+  model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mean_absolute_error', 'mean_squared_error'])
+  
+  return model
+  
+model = build_model()
+
+EPOCHS = 1000
+
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
+
+
+#Stop early
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+#history = model.fit(normed_train_data, train_labels, epochs=EPOCHS, validation_split = 0.2, verbose = 0, callbacks=[early_stop, PrintDot()])
+history = model.fit(train_dataset, train_labels, epochs=EPOCHS, validation_split = 0.2, verbose = 0, callbacks=[early_stop, PrintDot()])
+
+
+
+example_batch = train_dataset[:10]
+example_result = model.predict(example_batch)
+
+
+
+
+hist = pd.DataFrame(history.history)
+hist['epoch'] = history.epoch
+
+#test_predictions = model.predict(normed_test_data).flatten()
+test_predictions = model.predict(test_dataset).flatten()
+
+#loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose = 0)
+loss, mae, mse = model.evaluate(test_dataset, test_labels, verbose = 0)
+
+print("Testing set Mean Abs Error: {:5.2f}".format(mae))
+
+print("Labels")
+print(test_labels)
+
+print("Predictions")
+print(test_predictions)
